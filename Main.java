aebee0463c1c95f8ee18c3280af81f28dcd54be7
DannyM2027 <daniel.matut2027@gbwl.org>
2025-10-03 12:43:07 +0000

    1.8 notes

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+/**
+ *  @author Daniel M.
+ *  @since Oct 3 2025
+ *  @version 1.0
+ *  Notes 1.8-1.15
+ */
 public class Main {
+   /**
+    * main mehtod is entry point of the program
+    * @param input is an array of strings
+    * @return void means no OUTPUTT
+    */
 
    public static void main(String []args) {
       System.out.println("Hello World");
